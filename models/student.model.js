81596ddf7056e8332875c311abf0cf06effa84af
mazin189 <mazen.ahmed.abdelattii@gmail.com>
2026-07-16 19:25:32 +0300

    adding student model

---
+++ models/student.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose"
+
+const studentSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim:true
+    },
+    email : {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    courses: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Course"
+        }
+    ]
+})
+
+const Student = mongoose.model("Student", studentSchema)
+
+module.exports = Student
