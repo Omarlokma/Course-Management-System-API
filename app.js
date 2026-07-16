f44cde1c524f29d7f688ad011b87ef1357ea1323
Omarlokma <lokmax2@gmail.com>
2026-07-16 21:47:46 +0300

    Update course routes

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const connectDB = require("./config/db");
+import express from "express";
+import dotenv from "dotenv";
+
+import connectDB from "./config/db.js";
+
+import studentRoutes from "./routes/student.routes.js";
+import courseRoutes from "./routes/course.routes.js";
+import instructorRoutes from "./routes/instructor.routes.js";
 
 dotenv.config();
-const studentRoutes = require("./routes/student.routes.js")
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-const connectDB = require("./config/db");
 
 connectDB();
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Course Management API");
+  res.send("Course Management API");
 });
-app.use("/students", studentRoutes);
 
+app.use("/students", studentRoutes);
+app.use("/courses", courseRoutes);
+app.use("/instructors", instructorRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
-
--- routes/course.routes.js
+++ routes/course.routes.js
@@ -8,4 +8,8 @@ router.get("/:id", courseController.getCourseById);
 router.put("/:id", courseController.updateCourse);
 router.delete("/:id", courseController.deleteCourse);
 
+router.post("/:courseId/enroll", courseController.enrollStudent);
+router.get("/:courseId/students", courseController.getCourseStudents);
+router.get("/student/:studentId", courseController.getStudentCourses);
+
 export default router;
